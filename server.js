68c54dd6fb00fa6ced2cb2fc0b0425883448fc8d
adarsh <adarsh.2104@gmail.com>
2021-07-17 14:08:15 +0530

    Added code to connect to Mongo DB atlas

--- server.js
+++ server.js
@@ -7,19 +7,100 @@ const path = require('path')
 const ejs = require("ejs");
 const _ = require('lodash');
 const app = express();
+const {MongoClient} = require('mongodb');
 
 
-const mongoose = require('mongoose');
-const connection = "mongodb+srv://adarsh:2104@Cluster0/mongo?retryWrites=true&w=majority";
-mongoose.connect(connection, { useNewUrlParser: true,useUnifiedTopology: true, useFindAndModify: false });
+// const mongoose = require('mongoose');
+// const connection = "mongodb+srv://adarsh:2104@Cluster0/Cluster0?retryWrites=true&w=majority";
+// mongoose.connect(connection, { useNewUrlParser: true,useUnifiedTopology: true, useFindAndModify: false });
 
 
-const blogSchema = new mongoose.Schema({
-  title:String,
-  content:String
-});
+// const blogSchema = new mongoose.Schema({
+//   title:String,
+//   content:String
+// });
 
-const blog = mongoose.model("blog",blogSchema);
+// const blog = mongoose.model("blog",blogSchema);
+
+
+async function main(operationType){
+  const uri = "mongodb+srv://adarsh:2104@cluster0.xppwx.mongodb.net/mongo?retryWrites=true&w=majority";
+ 
+  const client = new MongoClient(uri,{ useNewUrlParser: true,useUnifiedTopology: true });
+  let result ;
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+      if (operationType === 'get_all'){
+          result = await showAllListing(client);
+      }
+      // Make the appropriate DB calls
+
+      // Create a single new listing
+      // await createListing(client,
+      //     {
+      //         name: "Lovely Loft",
+      //         summary: "A charming loft in Paris",
+      //         bedrooms: 1,
+      //         bathrooms: 1
+      //     }
+      // );
+
+      // Create 3 new listings
+      // await createMultipleListings(client, [
+      //     {
+      //         name: "Infinite Views",
+      //         summary: "Modern home with infinite views from the infinity pool",
+      //         property_type: "House",
+      //         bedrooms: 5,
+      //         bathrooms: 4.5,
+      //         beds: 5
+      //     },
+      //     {
+      //         name: "Private room in London",
+      //         property_type: "Apartment",
+      //         bedrooms: 1,
+      //         bathroom: 1
+      //     },
+      //     {
+      //         name: "Beautiful Beach House",
+      //         summary: "Enjoy relaxed beach living in this house with a private beach",
+      //         bedrooms: 4,
+      //         bathrooms: 2.5,
+      //         beds: 7,
+      //         last_review: new Date()
+      //     }
+      // ]);
+  } finally {
+      // Close the connection to the MongoDB cluster
+      await client.close();
+      return result;
+  }
+}
+
+main().catch(console.error);
+
+async function showAllListing(client){
+  // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#insertMany for the insertMany() docs
+  const result = client.db("mongo").collection("blogs").find({});
+
+
+  const data = await result.toArray();
+  // console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
+  // console.log(data);
+  return data;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 const homeStartingContent = "They are men of steel, standing tall in the harshest of conditions. They disregard freezing cold temperatures and scorching heat to always remain brave, awake and devoted towards us.They are all heroes, each and every one of them. But there are a few whose stories have become the stuff of legends, stories that deserve to be shared and told over and over again:";
 const aboutContent = "We believe that the support of our readers and followers are very crucial in attaining our objectives as a source of crisp and clear information. We thank you all for all the support and love that you have been showering us with. And we would like to iterate that the feeling is mutual. We hope to continue this relation in the many years to come.Please share your stories with our writers cover something in detail, please feel free to reach out to us on our email mentioned in the https://www.thebravehearts.in/contact section.";
@@ -35,13 +116,16 @@ app.use(express.static("public"));
 
 app.route("/")
 .get((req,res)=>{
-  let posts = []
-  blog.find((err,post)=>{
-    if (!err){
-      posts = post;
-    };
-    res.render("home",{homeStartingContent:homeStartingContent,posts:posts});
-  })
+  main('get_all').then(posts => {
+    console.log('data=',posts);
+    res.render("home",{homeStartingContent:homeStartingContent,posts:posts})
+  }).catch(console.error);
+  // blog.find((err,post)=>{
+  //   if (!err){
+  //     posts = post;
+  //   };
+  
+  // })
 
 });
 
